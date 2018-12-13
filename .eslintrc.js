c195dd9f6c45e6bd0a2097efe2f9be6b91693d1d
Mattias Hagberg <hello@mattias.pw>
2018-12-13 17:08:07 +0100

    Upgrading npm packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     env: {
         browser: true,
-        es6: true
+        es6: true,
+        node: true
     },
     extends: ['plugin:vue/recommended', 'eslint:recommended'],
     plugins: [
