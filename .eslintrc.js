bab53d07d2dd0168a38fe6f7aa95f7fbd74cfd42
Mattias Hagberg <hello@mattias.pw>
2018-12-18 21:18:08 +0100

    Misc updates, adding Anime.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,71 +1,52 @@
 module.exports = {
-    root: true,
-    parserOptions: {
-        parser: 'babel-eslint',
-        sourceType: 'module'
-    },
-    env: {
-        browser: true,
-        es6: true,
-        node: true
-    },
-    extends: ['plugin:vue/recommended', 'eslint:recommended'],
-    plugins: [
-        'html'
-    ],
-    globals: {
-        'firebase': true,
-        'module': true
-    },
-    'rules': {
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'warn',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'no-console': 0,
-        'no-useless-escape': 0,
-        'comma-spacing': 'warn',
-        'comma-style': 'warn',
-        'space-infix-ops': [
-            'error',
-            {'int32Hint': true}
-        ],
-        'brace-style': 'warn',
-        'no-multiple-empty-lines': 'warn',
-        'func-call-spacing': [
-            'warn',
-            'never'
-        ],
-        'key-spacing': 'warn',
-        'no-array-constructor': 'error',
-        'no-new-object': 'error',
-        'no-duplicate-imports': 'error',
-        'no-multi-spaces': 'warn',
-        'no-path-concat': 'error',
-        'no-template-curly-in-string': 'warn',
-        'template-curly-spacing': 'warn',
-        'no-trailing-spaces': 'warn',
-        'object-property-newline': 'error',
-        'padded-blocks': [
-            'warn',
-            'never'
-        ],
-        'rest-spread-spacing': 'warn',
-        'semi-spacing': 'error',
-        'space-in-parens': 'error',
-        'spaced-comment': 'error'
-    }
-}
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+  plugins: ['html', 'vue'],
+  globals: {
+    firebase: true,
+    module: true
+  },
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['warn', 'single'],
+    semi: ['error', 'always'],
+    'no-console': 0,
+    'no-useless-escape': 0,
+    'comma-spacing': 'warn',
+    'comma-style': 'warn',
+    'space-infix-ops': ['error', { int32Hint: true }],
+    'brace-style': 'warn',
+    'no-multiple-empty-lines': 'warn',
+    'func-call-spacing': ['warn', 'never'],
+    'key-spacing': 'warn',
+    'no-array-constructor': 'error',
+    'no-new-object': 'error',
+    'no-duplicate-imports': 'error',
+    'no-multi-spaces': 'warn',
+    'no-path-concat': 'error',
+    'no-template-curly-in-string': 'warn',
+    'template-curly-spacing': 'warn',
+    'no-trailing-spaces': 'warn',
+    'object-property-newline': 'error',
+    'padded-blocks': ['warn', 'never'],
+    'rest-spread-spacing': 'warn',
+    'semi-spacing': 'error',
+    'space-in-parens': 'error',
+    'spaced-comment': 'error',
+    'vue/html-indent': ['error', 4],
+    'vue/max-attributes-per-line': 0,
+    'vue/attributes-order': 0,
+    'vue/singleline-html-element-content-newline': 0
+  }
+};
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
     <div id="app">
         <header>
-            <img src="./assets/logotype.svg" class="logotype">
+            <svg xmlns="http://www.w3.org/2000/svg" width="612" height="612" viewBox="665.573 0 612 612" class="logotype">
+                <path fill="none" stroke="#1c140d" stroke-width="2.9999700000000002" d="M155.335 216.8L305.997 66.137l150.662 150.661-150.662 150.662z" />
+                <path fill="none" stroke="#1c140d" stroke-width="3" d="M359.75 265.167L306 367.462l-54.5-102.295 54.5-48.368z" />
+                <path fill="none" stroke="#1c140d" stroke-width="3" d="M454 244.804L306 552.49 157.5 244.772 306 159.166z" />
+                <g fill="none" stroke="#1c140d" stroke-width="3">
+                    <path d="M551.134 267.515L323 551.49l153.5-307.657 56.667-35.667zM535.167 184.2l-52.05 32.599L331.75 66.25l95.002-8.431z" />
+                </g>
+                <g fill="none" stroke="#1c140d" stroke-width="3">
+                    <path d="M60.866 267.515L289 551.49 135.5 243.833l-56.667-35.667zM76.833 184.2l52.05 32.599L280.25 66.25l-95.002-8.431z" />
+                </g>
+                <g>
+                    <path stroke="#fff" stroke-width="10" d="M1119.573 244.804l-148 307.686-148.5-307.718 148.5-85.606z" />
+                    <path stroke="#fff" stroke-width="9.9999" d="M820.902 216.803L971.564 66.142l150.661 150.661-150.661 150.662z" />
+                    <path fill="none" stroke="#fff" stroke-width="10" d="M823.073 244.772l148.5-85.606 148 85.638" />
+                    <path stroke="#fff" stroke-width="10" d="M1025.179 265.167l-53.75 102.295-54.5-102.295 54.5-48.368z" />
+                    <g stroke="#fff" stroke-width="3">
+                        <path d="M1216.706 267.515L988.573 551.49l153.5-307.657 56.667-35.667zM1200.74 184.2l-52.05 32.599L997.323 66.25l95.001-8.431z" />
+                    </g>
+                    <g stroke="#fff" stroke-width="3">
+                        <path d="M726.439 267.515L954.573 551.49l-153.5-307.657-56.667-35.667zM742.406 184.2l52.049 32.599L945.823 66.25l-95.002-8.431z" />
+                    </g>
+                </g>
+            </svg>
             <img src="./assets/logo.svg">
             <h1>
                 {{ subtitle }}
@@ -33,14 +55,16 @@
                     </li>
                 </ul>
                 <ul class="beer-recipe" :id="beer.id">
-                    <li><strong>Ingredients:</strong>
+                    <li>
+                        <strong>Ingredients:</strong>
                         <ul>
                             <li v-for="ingredient in beer.ingredients.slice(1)" :key="ingredient">
                                 {{ ingredient }}
                             </li>
                         </ul>
                     </li>
-                    <li><strong>Instructions:</strong>
+                    <li>
+                        <strong>Instructions:</strong>
                         <ol>
                             <li v-for="instruction in beer.instructions.slice(1)" :key="instruction">
                                 {{ instruction }}
@@ -71,52 +95,61 @@
 </template>
 
 <script>
-    import firebase from 'firebase';
-    import * as config from '../firebase.config';
+import firebase from 'firebase';
+import * as config from '../firebase.config';
+import anime from 'animejs';
 
-    firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-    export default {
-        name: 'app',
-        data() {
-            return {
-                subtitle: 'Craft Brewery',
-                beers: []
-            }
-        },
-        mounted() {
-            this.loadBeers();
-        },
-        methods: {
-            loadBeers() {
-                const db = firebase.database();
+export default {
+    name: 'App',
+    data() {
+        return {
+            subtitle: 'Craft Brewery',
+            beers: []
+        };
+    },
+    mounted() {
+        this.loadBeers();
 
-                db.ref('/').once('value').then(snap => {
-                    let promises = [];
+        anime({
+            targets: '.logotype g path',
+            easing: 'easeInOutSine',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            duration: 1500,
+            delay: 1500
+        });
+    },
+    methods: {
+        loadBeers() {
+            const db = firebase.database();
 
-                    snap.forEach(beer => {
-                        promises.push(
-                            this.beers.push(beer.val())
-                        );
-                    });
+            db.ref('/').once('value').then(snap => {
+                let promises = [];
 
-                    Promise.all(promises).then(() =>
-                        document.getElementById('loading').remove()
+                snap.forEach(beer => {
+                    promises.push(
+                        this.beers.push(beer.val())
                     );
                 });
-            },
-            showRecipe(id) {
-                const el = document.getElementById(id);
 
-                el.style.display === 'block' ?
-                    el.style.display = 'none' :
-                    el.style.display = 'block';
-            }
+                Promise.all(promises).then(() =>
+                    document.getElementById('loading').remove()
+                );
+            });
+        },
+        showRecipe(id) {
+            const el = document.getElementById(id);
+
+            el.style.display === 'block'
+                ? (el.style.display = 'none')
+                : (el.style.display = 'block');
         }
     }
+};
 </script>
 
 <style>
-    @import url('https://fonts.googleapis.com/css?family=Allerta+Stencil|Lato');
-    @import url('main.css');
+@import url('https://fonts.googleapis.com/css?family=Allerta+Stencil|Lato');
+@import url('main.css');
 </style>
