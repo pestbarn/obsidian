5e7e71a4ee8383c1f93d3e12551dbdb806ddf320
Mattias Hagberg <hello@mattias.pw>
2019-02-28 09:50:34 +0100

    Major style update

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,18 @@
     <div id="app">
         <header>
             <svg xmlns="http://www.w3.org/2000/svg" width="612" height="612" viewBox="665.573 0 612 612" class="logotype">
-                <path fill="none" stroke="#1c140d" stroke-width="2.9999700000000002" d="M155.335 216.8L305.997 66.137l150.662 150.661-150.662 150.662z" />
-                <path fill="none" stroke="#1c140d" stroke-width="3" d="M359.75 265.167L306 367.462l-54.5-102.295 54.5-48.368z" />
-                <path fill="none" stroke="#1c140d" stroke-width="3" d="M454 244.804L306 552.49 157.5 244.772 306 159.166z" />
-                <g fill="none" stroke="#1c140d" stroke-width="3">
-                    <path d="M551.134 267.515L323 551.49l153.5-307.657 56.667-35.667zM535.167 184.2l-52.05 32.599L331.75 66.25l95.002-8.431z" />
-                </g>
-                <g fill="none" stroke="#1c140d" stroke-width="3">
+                <g fill="none" stroke="#1c140d" stroke-width="7">
                     <path d="M60.866 267.515L289 551.49 135.5 243.833l-56.667-35.667zM76.833 184.2l52.05 32.599L280.25 66.25l-95.002-8.431z" />
                 </g>
                 <g>
-                    <path stroke="#fff" stroke-width="10" d="M1119.573 244.804l-148 307.686-148.5-307.718 148.5-85.606z" />
-                    <path stroke="#fff" stroke-width="9.9999" d="M820.902 216.803L971.564 66.142l150.661 150.661-150.661 150.662z" />
-                    <path fill="none" stroke="#fff" stroke-width="10" d="M823.073 244.772l148.5-85.606 148 85.638" />
-                    <path stroke="#fff" stroke-width="10" d="M1025.179 265.167l-53.75 102.295-54.5-102.295 54.5-48.368z" />
-                    <g stroke="#fff" stroke-width="3">
+                    <path fill="#181818" stroke="#916947" stroke-width="7" d="M1119.573 244.804l-148 307.686-148.5-307.718 148.5-85.606z" />
+                    <path fill="#181818" stroke="#916947" stroke-width="7" d="M820.902 216.803L971.564 66.142l150.661 150.661-150.661 150.662z" />
+                    <path fill="#181818" stroke="#916947" stroke-width="7" d="M823.073 244.772l148.5-85.606 148 85.638" />
+                    <path fill="#181818" stroke="#916947" stroke-width="7" d="M1025.179 265.167l-53.75 102.295-54.5-102.295 54.5-48.368z" />
+                    <g fill="#181818" stroke="#916947" stroke-width="7">
                         <path d="M1216.706 267.515L988.573 551.49l153.5-307.657 56.667-35.667zM1200.74 184.2l-52.05 32.599L997.323 66.25l95.001-8.431z" />
                     </g>
-                    <g stroke="#fff" stroke-width="3">
+                    <g fill="#181818" stroke="#916947" stroke-width="7">
                         <path d="M726.439 267.515L954.573 551.49l-153.5-307.657-56.667-35.667zM742.406 184.2l52.049 32.599L945.823 66.25l-95.002-8.431z" />
                     </g>
                 </g>
@@ -115,13 +109,11 @@ export default {
         this.loadBeers();
 
         anime({
-            targets: '.logotype g path',
+            targets: '.logotype path',
             easing: 'easeInOutSine',
             strokeDashoffset: [ anime.setDashoffset, 0 ],
-            duration: (el, i) => i * 250,
-            direction: 'alternate',
-            delay: (el, i) => i * 250,
-            loop: true
+            duration: (el, i) => i * 750,
+            delay: 1500
         });
     },
     methods: {
