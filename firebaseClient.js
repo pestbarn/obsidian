4246a0db1807fdd9d503dc361b4cb0c888c298d2
Mattias Hagberg <hello@mattias.pw>
2019-11-15 16:58:18 +0100

    Added database ref to node

--- firebaseClient.js
+++ firebaseClient.js
@@ -1,9 +1,15 @@
 // https://howtofirebase.com/save-and-query-firebase-data-ed73fb8c6e3a
-const admin = require('firebase-admin');
+const firebase = require('firebase-admin');
 
 const serviceAccount = require('./serviceAccountKey.json');
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
     databaseURL: 'https://obsidian-beer.firebaseio.com'
 });
+
+const ref = firebase.app().database().ref();
+ref.once('value')
+    .then(function(snap) {
+        console.log('snap.val()', snap.val());
+    });
