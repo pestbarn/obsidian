0d61863c42ca3412a4fe8e0458e4ab9bdeec1551
Mattias Hagberg <pestbarn@gmail.com>
2021-01-23 00:41:19 +0100

    Updates to beer info

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
                     <path d="M60.866 267.515L289 551.49 135.5 243.833l-56.667-35.667zM76.833 184.2l52.05 32.599L280.25 66.25l-95.002-8.431z" />
                 </g>
                 <g>
-                    <path fill="#181818" stroke="#A4663F" stroke-width="9" d="M1119.573 244.804l-148 307.686-148.5-307.718 148.5-85.606z" />
-                    <path fill="#181818" stroke="#A4663F" stroke-width="9" d="M820.902 216.803L971.564 66.142l150.661 150.661-150.661 150.662z" />
-                    <path fill="#181818" stroke="#A4663F" stroke-width="9" d="M823.073 244.772l148.5-85.606 148 85.638" />
-                    <path fill="#181818" stroke="#A4663F" stroke-width="9" d="M1025.179 265.167l-53.75 102.295-54.5-102.295 54.5-48.368z" />
-                    <g fill="#181818" stroke="#A4663F" stroke-width="9">
+                    <path fill="#181818" stroke="#a4663f" stroke-width="9" d="M1119.573 244.804l-148 307.686-148.5-307.718 148.5-85.606z" />
+                    <path fill="#181818" stroke="#a4663f" stroke-width="9" d="M820.902 216.803L971.564 66.142l150.661 150.661-150.661 150.662z" />
+                    <path fill="#181818" stroke="#a4663f" stroke-width="9" d="M823.073 244.772l148.5-85.606 148 85.638" />
+                    <path fill="#181818" stroke="#a4663f" stroke-width="9" d="M1025.179 265.167l-53.75 102.295-54.5-102.295 54.5-48.368z" />
+                    <g fill="#181818" stroke="#a4663f" stroke-width="9">
                         <path d="M1216.706 267.515L988.573 551.49l153.5-307.657 56.667-35.667zM1200.74 184.2l-52.05 32.599L997.323 66.25l95.001-8.431z" />
                     </g>
-                    <g fill="#181818" stroke="#A4663F" stroke-width="9">
+                    <g fill="#181818" stroke="#a4663f" stroke-width="9">
                         <path d="M726.439 267.515L954.573 551.49l-153.5-307.657-56.667-35.667zM742.406 184.2l52.049 32.599L945.823 66.25l-95.002-8.431z" />
                     </g>
                 </g>
@@ -55,11 +55,10 @@
                         </li>
                         <li>
                             {{ typeof beer.abv === 'string' ? beer.abv : beer.abv[beer.currentbatch] }}
-                            <span v-if="beer.ibu" style="display: none;"> - {{ typeof beer.abv === 'string' ? beer.ibu : beer.ibu[beer.currentbatch] }} IBU</span>
+                            <span v-if="beer.ibu"> - {{ typeof beer.abv === 'string' ? beer.ibu : beer.ibu[beer.currentbatch] }} IBU</span>
                         </li>
                         <li v-if="beer.currentbatch">
-                            <em v-if="beer.onetime">(one-off brew)</em>
-                            <em v-else>Current batch: {{ beer.currentbatch }}</em>
+                            <em v-if="!beer.onetime">Current batch: {{ beer.currentbatch }}</em>
                         </li>
                         <li v-if="beer.description">
                             &mdash;
@@ -80,7 +79,7 @@
                         <li>
                             <h3>
                                 How to brew {{ beer.name }}
-                                <span v-if="beer.currentbatch !== '1'">(batch {{ beer.currentbatch }})</span>
+                                <span v-if="beer.currentbatch && beer.currentbatch > 1">(batch {{ beer.currentbatch }})</span>
                             </h3>
                         </li>
                         <li>
