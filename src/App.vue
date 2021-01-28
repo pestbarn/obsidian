7bd3fc85a471c692134e82bceb8cb12d3a2191c8
Mattias Hagberg <pestbarn@gmail.com>
2021-01-28 22:02:51 +0100

    Break out logo to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
 <template>
     <div id="app">
         <header>
-            <svg xmlns="http://www.w3.org/2000/svg" width="612" height="612" viewBox="665.573 0 612 612" class="logotype">
-                <g fill="none" stroke="#1c140d" stroke-width="7">
-                    <path d="M60.866 267.515L289 551.49 135.5 243.833l-56.667-35.667zM76.833 184.2l52.05 32.599L280.25 66.25l-95.002-8.431z" />
-                </g>
-                <g>
-                    <path fill="#181818" stroke="#a4663f" stroke-width="9" d="M1119.573 244.804l-148 307.686-148.5-307.718 148.5-85.606z" />
-                    <path fill="#181818" stroke="#a4663f" stroke-width="9" d="M820.902 216.803L971.564 66.142l150.661 150.661-150.661 150.662z" />
-                    <path fill="#181818" stroke="#a4663f" stroke-width="9" d="M823.073 244.772l148.5-85.606 148 85.638" />
-                    <path fill="#181818" stroke="#a4663f" stroke-width="9" d="M1025.179 265.167l-53.75 102.295-54.5-102.295 54.5-48.368z" />
-                    <g fill="#181818" stroke="#a4663f" stroke-width="9">
-                        <path d="M1216.706 267.515L988.573 551.49l153.5-307.657 56.667-35.667zM1200.74 184.2l-52.05 32.599L997.323 66.25l95.001-8.431z" />
-                    </g>
-                    <g fill="#181818" stroke="#a4663f" stroke-width="9">
-                        <path d="M726.439 267.515L954.573 551.49l-153.5-307.657-56.667-35.667zM742.406 184.2l52.049 32.599L945.823 66.25l-95.002-8.431z" />
-                    </g>
-                </g>
-            </svg>
+            <ObsidianLogo></ObsidianLogo>
             <img src="assets/logo.svg">
             <h1>
                 {{ subtitle }}
@@ -112,7 +96,7 @@
                     <VuePureLightbox
                         :thumbnail="`${images[beer.id]}`"
                         :images="[`${fullImage[beer.id]}`]"
-                    ></VuePureLightbox>
+                    />
                 </figure>
             </article>
         </div>
@@ -120,6 +104,7 @@
 </template>
 
 <script>
+import ObsidianLogo from './components/ObsidianLogo.vue';
 import VuePureLightbox from 'vue-pure-lightbox';
 import firebase from 'firebase/app';
 import 'firebase/database';
@@ -135,6 +120,10 @@ if (!firebase.apps.length) {
 
 export default {
     name: 'App',
+    components: {
+        VuePureLightbox,
+        ObsidianLogo
+    },
     data() {
         return {
             subtitle: 'Craft Brewery',
@@ -142,9 +131,6 @@ export default {
             images: images,
             fullImage: fullImage
         };
-    },
-    components: {
-        VuePureLightbox
     },
     mounted() {
         this.loadBeers();
